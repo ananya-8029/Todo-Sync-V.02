de5382d45f8d553749db097ff157da0f690d35e0
Ananya Gupta <anaya29003@gmail.com>
2024-07-05 23:08:52 +0530

    Update Changes

--- my-todo-list-backend/db.js
+++ my-todo-list-backend/db.js
@@ -4,6 +4,7 @@ const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI); 
+  console.log("Connected Successfully")
 };
 
 module.exports = connectToMongo;
