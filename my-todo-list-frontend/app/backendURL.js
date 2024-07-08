89275c27366982027861935cfe93fc2beef85b93
Ananya Gupta <anaya29003@gmail.com>
2024-07-08 15:57:26 +0530

    Update Changes

--- my-todo-list-frontend/app/backendURL.js
+++ my-todo-list-frontend/app/backendURL.js
@@ -1,6 +1,6 @@
 const backendURL = {
-  // backendUrl: "https://todo-sync-v-02-backend.onrender.com",
-  backendUrl: "http://localhost:5000"
+  backendUrl: "https://todo-sync-v-02-backend.onrender.com",
+  // backendUrl: "http://localhost:5000"
 };
 
 export default backendURL;
